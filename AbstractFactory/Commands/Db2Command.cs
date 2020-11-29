b59ed36544161c44023b6d0572283d85b4b86b5f
Berkay Bozkurt <BozkurtBerkay@hotmail.com>
2020-11-29 17:25:52 +0300

    Add project files.

---
+++ AbstractFactory/Commands/Db2Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    public class Db2Command : Command
+    {
+        public override void Execute(string query)
+        {
+            Console.WriteLine("Db2 sorgusu çalıştırılır.");
+        }
+    }
+}
---
+++ AbstractFactory/Factories/InterBaseFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    public class InterBaseFactory : DatabaseFactory
+    {
+        public override Command CreateCommand()
+        {
+            return new InterBaseCommand();
+        }
+
+        public override Connection CreateConnection()
+        {
+            return new InterBaseConnection();
+        }
+    }
+}
